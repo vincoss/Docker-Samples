781a233af4d8938cf305d196e1be74b0bf476de6
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-06 03:33:44 +1000

    update sample

--- src/DockerSamples/Jenkins0001/executors.groovy
+++ src/DockerSamples/Jenkins0001/executors.groovy
@@ -1,2 +1,9 @@
-﻿import jenkins.model.*
-Jenkins.instance.setNumExecutors(5)
+﻿import jenkins.model.Jenkins
+
+Integer numberOfExecutors = 5
+
+Jenkins jenkins = Jenkins.getInstance()
+
+jenkins.setNumExecutors(numberOfExecutors)
+
+jenkins.save()
