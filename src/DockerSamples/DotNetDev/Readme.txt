0db149a893c21b5b92e71040d7f3f80aec40b538
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-18 17:00:28 +1000

    build tools

--- src/DockerSamples/DotNetDev/Readme.txt
+++ src/DockerSamples/DotNetDev/Readme.txt
@@ -6,3 +6,10 @@ https://github.com/microsoft/dotnet-framework-docker
 
 # Build docker image
 docker build --no-cache -t vincoss/dotnetdev:1.0.0 .
+
+## Issues
+
+1. Installing TeamCity Agent might fail if the service user does not have righs to "Log on as service".
+	https://superuser.com/questions/747094/easiest-way-to-grant-log-on-as-a-service-to-a-windows-user-from-the-command-li
+
+	
--- src/DockerSamples/DotNetDev/createUser.ps1
+++ src/DockerSamples/DotNetDev/createUser.ps1
@@ -2,3 +2,8 @@ $Password = Get-Content C:\temp\passwordEncrypted.txt | ConvertTo-SecureString -
 New-LocalUser "TeamCityAgentUser" -Password $Password -FullName "TeamCityAgentUser" -Description "Account for TeamCity Agent." 
 Add-LocalGroupMember -Group "Administrators" -Member "TeamCityAgentUser"
 Set-LocalUser -Name "TeamCityAgentUser" -PasswordNeverExpires 1
+
+<#
+	.".\AddAccountToLogonAsService.ps1" "DOMAIN\Account"
+#>
+& "$PSScriptRoot\AddAccountToLogonAsService.ps1" "TeamCityAgentUser"
